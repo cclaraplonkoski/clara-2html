31c96621aeaf798aae4910fdbf3ee314e60aa9df
cclaraplonkoski <clarapm2014@gmail.com>
2025-12-01 09:08:23 -0300

    Add files via upload
    gf m

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const avanca = document.querySelectorAll('.btn-proximo');
-avanca.forEach(button => {
-button.addEventListener('click', function(){
-const atual = document.querySelector('.ativo');
-const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+const botoes = document.querySelectorAll(".btn-proximo");
+const passos = document.querySelectorAll(".passo");
 
-atual.classList.remove('ativo');
-document.getElementById(proximoPasso).classList.add('ativo');
-})
-})
+botoes.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const destino = btn.getAttribute("data-proximo");
+
+        passos.forEach(p => p.classList.remove("ativo"));
+        document.getElementById(`passo-${destino}`).classList.add("ativo");
+    });
+});
